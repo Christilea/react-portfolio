73159484e76ec35512ceada771a095e52d26e501
Christi <mahomes4president@gmail.com>
2021-08-06 13:39:04 -0500

    adding style

--- src/index.js
+++ src/index.js
@@ -4,12 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+const myelement = (
+  <ul>
+    <li>apples</li>
+    <li>Bananas</li>
+    <li>Cherries</li>
+  </ul>
+);
+
+ReactDOM.render(myelement, document.getElementById('root'));
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -35,3 +35,4 @@ const projectARR = [
         </div>
     )
 }
+
