78c316901753610e59aa9bd6d3e4331567c8d0e3
Christi <mahomes4president@gmail.com>
2021-08-09 19:52:03 -0500

    adding style and link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function NavBar() {
     return (
-        <div>
+        <div className = 'nav'>
         <Link to='/' >
             Home
         </Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import portfoliophoto from '../photos/Portfoliopic.jpg'
 
 export default function Home() {
     return (
-        <div>
+        <div className = 'homepage'>
+            <img  src = {portfoliophoto} alt = 'headshot'/> 
         <h1>Christi Lea - Web Developer</h1>
             <h2>Portfolio 2021</h2>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+
         </div>
     )
 }
