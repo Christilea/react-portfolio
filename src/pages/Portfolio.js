d63e0dbbf58e8a4561aace048eb797ba0853dfb9
Christi <mahomes4president@gmail.com>
2021-08-14 17:13:45 -0500

    deleting project not working and adding a new one

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,23 +3,24 @@ import PortfolioItem from '../components/PortfolioItem'
 
 export default function Portfolio() {
 const projectARR = [
-    {
-        title:'DonutApp', 
-        github: 'https://github.com/erodriguez541/donutProject', 
-        deployed: 'https://erodriguez541.github.io/donutProject/',
-        description: 'Find a donut nearby'
-    },
+    
     {
         title:'PasswordGenerator', 
         github: 'https://github.com/Christilea/Random_Password_Generator', 
         deployed: 'https://christilea.github.io/Random_Password_Generator/',
-        description: 'password generator'
+        description: 'Generate a password'
     },
     {
         title:'Online-Offline Budget Tracker', 
         github: 'https://github.com/Christilea/Online_Offline-BudgetTracker', 
         deployed: 'https://aqueous-sierra-82642.herokuapp.com/',
-        description: 'Budget Tracker App'
+        description: 'Track your budget. Online and Offline'
+    },
+    {
+        title:'API_Weatherapp', 
+        github: 'https://github.com/Christilea/API_Weatherapp', 
+        deployed: 'https://christilea.github.io/API_Weatherapp/',
+        description: 'See the weather for the next 5 days!'
     },
 ]
     return (
